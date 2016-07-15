2bb8be5db8fa634d4606d0993f4c710bc7d8f9c2
Patrick Todd <patrick.a.todd@gmail.com>
2016-07-15 12:55:34 -0400

    updated server to accept edits

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def index():
 
 @app.route('/friends', methods=['POST'])
 def create():
-    query = "INSERT INTO friends (first_name, last_name,created_at, updated_at) VALUES (:first_name, :last_name, NOW(), NOW())"
+    query = "INSERT INTO friends (first_name, last_name, created_at, updated_at) VALUES (:first_name, :last_name, NOW(), NOW())"
     data = {
              'first_name': request.form['first_name'], 
              'last_name':  request.form['last_name'],
@@ -22,22 +22,22 @@ def create():
 
 @app.route('/friends/<id>', methods=['POST'])
 def update(id):
-    query = "UPDATE friends SET first_name = :first_name, last_name = :last_name WHERE id = :id"
     data = {
              'first_name': request.form['first_name'], 
              'last_name':  request.form['last_name'],
-             'id': friend_id
+             'id': id
            }
+    query = "UPDATE friends SET first_name = :first_name, last_name = :last_name WHERE id = :id"
     mysql.query_db(query, data)
     return redirect('/')
 
 
 @app.route('/friends/<id>/edit', methods=['GET'])
 def edit(id):
-    query = "SELECT * FROM friends WHERE id = :specific_id"
-    data = {'specific_id': id}
-    friends = mysql.query_db(query, data)
-    return render_template('friends.html', one_friend=friends[0])
+    data = {
+    'id': id
+    }
+    return render_template('friends.html', id=id)
 
 
 @app.route('/friends/<id>/delete', methods=['POST'])
